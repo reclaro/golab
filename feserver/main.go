285d1ef11163ec476c7122d9ac22ff6bf79dc92f
Andrea Rosa <andrea.rosa@gmail.com>
2019-03-15 21:55:17 +0000

    frontend

--- feserver/main.go
+++ feserver/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
-	"github.com/hoisie/web"
+
+	"github.com/gin-gonic/gin"
+	"github.com/reclaro/golab/ghapi"
 )
 
-func hello() string {
-	return "hello"
-}
-
 func main() {
-	web.Get("/(.*)", hello)
-	//web.Run("0.0.0.0:9999")
+	// Parameters are passed in the querystring
+	//http://localhost:8080/?name=fnproject/fn&search=TODO
 	fmt.Println("Started")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		repoName := c.Query("name")
+		search := c.Query("search")
+		sq := ghapi.NewSearchQuery(repoName, search)
+		result := ghapi.Search(sq)
+		c.JSON(200, result)
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
--- ghapi/search.go
+++ ghapi/search.go
@@ -3,11 +3,37 @@ package ghapi
 import (
 	"context"
 	"fmt"
+
 	"github.com/google/go-github/github"
 )
 
-// Search executes a search for a term inside a specified repositroy name
-func Search(searchTerm, repoName string) (*github.CodeSearchResult, error) {
+//SearchQuery is used to pass the repository name and the query we want to perform
+type SearchQuery struct {
+	Repo  string `json:"repo_name"`
+	Query string `json:"query"`
+}
+
+type msgResult struct {
+	Total    int    `json:"total_occurences,omitempty"`
+	FileName string `json:"file_name,omitempty"`
+	FilePath string `json:"file_path,omitempty"`
+	HTMLURL  string `json:"html_url,omitempty"`
+}
+
+// SearchResults returns the results of the query
+type SearchResults struct {
+	Error   string       `json:"error"`
+	Results []*msgResult `json:"results"`
+}
+
+// NewSearchQuery creates a new SearchQuery
+func NewSearchQuery(reponame, searchTerm string) *SearchQuery {
+	return &SearchQuery{Repo: reponame,
+		Query: searchTerm}
+
+}
+
+func doSearch(searchTerm, repoName string) (*github.CodeSearchResult, error) {
 	searchString := fmt.Sprintf("%s in:file repo:%s", searchTerm, repoName)
 	client := github.NewClient(nil)
 	ctx := context.Background()
@@ -16,4 +42,29 @@ func Search(searchTerm, repoName string) (*github.CodeSearchResult, error) {
 		return nil, err
 	}
 	return results, nil
+}
+
+// Search executes a search for a term inside a specified repositroy name
+func Search(sq *SearchQuery) SearchResults {
+	var msg SearchResults
+	resu, err := doSearch(sq.Query, sq.Repo)
+	if err != nil {
+		msg = SearchResults{Error: fmt.Sprintf("No results found. %s ", err.Error())}
+	} else {
+		results := make([]*msgResult, 0)
+		if len(resu.CodeResults) > 0 {
+			recapResult := &msgResult{Total: *resu.Total}
+			results = append(results, recapResult)
+		}
+		for _, v := range resu.CodeResults {
+			r := &msgResult{
+				FileName: *v.Name,
+				FilePath: *v.Path,
+				HTMLURL:  *v.HTMLURL,
+			}
+			results = append(results, r)
+		}
+		msg = SearchResults{Results: results}
+	}
+	return msg
 }
