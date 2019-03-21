ff516f765ebadd825317f1686823428b503d5f7a
Andrea Rosa <andrea.rosa@oracle.com>
2019-03-21 09:54:28 +0000

    versione senza parametri

--- feserver/main.go
+++ feserver/main.go
@@ -8,14 +8,10 @@ import (
 )
 
 func main() {
-	// Parameters are passed in the querystring
-	//http://localhost:8080/?name=fnproject/fn&search=TODO
 	fmt.Println("Started")
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		repoName := c.Query("name")
-		search := c.Query("search")
-		sq := ghapi.NewSearchQuery(repoName, search)
+		sq := ghapi.NewSearchQuery("fnproject/fn", "TODO")
 		result := ghapi.Search(sq)
 		c.JSON(200, result)
 	})
